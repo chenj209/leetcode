bd38db4d43f2a2cecd0d02065ec0528fa4fd58b0
chenj209 <kinderjianda.chen@gmail.com>
2021-08-04 23:39:49 -0400

    gloria build tree algorithm

--- binary_tree.py
+++ binary_tree.py
@@ -11,21 +11,26 @@ def print_tree_helper(root, level):
         return ""
 
     tree_str = ""
-    if root.left:
-        tree_str += print_tree_helper(root.left, level+1)
+    if root.right:
+        tree_str += print_tree_helper(root.right, level+1)
 
     level_indent = "    " * level
     tree_str += level_indent + f"->{root.val}\n"
 
-    if root.right:
-        tree_str += print_tree_helper(root.right, level+1)
+    if root.left:
+        tree_str += print_tree_helper(root.left, level+1)
 
     return tree_str
 
 class BinaryTree:
     def __init__(self, val_lst):
+        self.root = TreeNode(val_lst[0])
+        build_tree(val_lst, self.root, 0)
+
+    def __init__2(self, val_lst):
         """
         val_lst: [value] where value are sorted in layer order from left to right. Layer i always has 2^i nodes (empty nodes represented using None), except the last layer
+        e.g.: [1#root,2,3#layer1, 4,5,6,7#layer2,...]
         """
         self.root = TreeNode(val_lst[0])
 
@@ -59,6 +64,27 @@ class BinaryTree:
 
     def print_tree(self):
         return print_tree_helper(self.root, 0)
+
+def build_tree(val_lst, parent, cur_index):
+    # base case 1: cur_index represents out of bound node
+    if cur_index == len(val_lst):
+        # return current parent
+        return parent
+    # base case 2: parent is None
+    if parent is None:
+        return None
+
+    # general case: 0 <= cur_index < len(val_lst), parent is not None
+    if 2 * cur_index + 1 < len(val_lst):
+        # TreeNode(val)
+        parent.left = TreeNode(val_lst[cur_index * 2 + 1])
+        parent.left = build_tree(val_lst, parent.left, cur_index * 2 + 1)
+    if 2 * cur_index + 2 < len(val_lst):
+        parent.right = TreeNode(val_lst[cur_index * 2 + 2])
+        parent.right = build_tree(val_lst, parent.right, cur_index * 2 + 2)
+
+    return parent
+
 
 
 
